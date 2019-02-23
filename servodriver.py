c41a995580cb3876b8bea5bae62575d6f4137d6d
Kyle <kkingsbe@gmail.com>
2019-02-23 12:47:40 -0500

    Finished Servo Control Library

--- servodriver.py
+++ servodriver.py
@@ -1,7 +1,10 @@
 import RPi.GPIO as GPIO #Official GPIO package
 
-GPIO.setmode(GPIO.BCM)
+# https://raspberrypi.stackexchange.com/questions/12966/what-is-the-difference-between-board-and-bcm-for-gpio-pin-numbering
+# http://www.datasheetcafe.com/hs-55-datasheet-servo-motor/
+# https://rpi.science.uoit.ca/lab/servo/
 
+GPIO.setmode(GPIO.BCM)
 class servo:
     def __init__(self, pin, zeropulsewidth, fullextensionpulsewidth, maxtravel):
         self.pin = pin
@@ -11,19 +14,11 @@ class servo:
         GPIO.setup(self.pin, GPIO.OUT)
         self.moveservo = GPIO.PWM(self.pin, 50)  # The servo is expecting a pulse every 20mms or 50hz
     def setangle(self, angle):
-        #BROKEN
-        # https://www.servocity.com/hitec-hs-55-servo
         print("Angle: " + str(angle))
-        #self.dutycycle = 4.5 + ((float(angle) / 90) * 5)
         self.dutycycle = 100 * (self.zeropulsewidth + (angle * ((self.fullextensionpulsewidth - self.zeropulsewidth) / self.maxtravel))) / 20
         self.moveservo.start(self.dutycycle)
         print("MS: " + str(self.dutycycle * .2))
         print("Duty Cycle: " + str(self.dutycycle) + "%")
         print("----------------")
-    def setdc(self, dc):
-        self.dutycycle = float(dc / 20) * 100
-        self.moveservo.start(self.dutycycle)
-        #time.sleep(2)
-        print("Duty Cycle: " + str(self.dutycycle))
     def cleanup(self):
         GPIO.cleanup()
